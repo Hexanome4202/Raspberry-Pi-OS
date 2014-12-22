16a461c2685d1e1094fbe2c8e597502efb90c9cd
MartinRemi <mrmi11@yahoo.fr>
2014-12-22 13:56:15 +0100

    made a few fixes

--- ospie-start/sem.c
+++ ospie-start/sem.c
@@ -1,15 +1,27 @@
 #include "sem.h"
+#include "sched.h"
+#include "hw.h"
 
 void sem_init(sem_s* sem, unsigned int val) {
 	sem->counter = val;
 }
 
 void sem_up(sem_s* sem) {
+	DISABLE_IRQ();
+
 	++(sem->counter);
 	// Elect one process in the queue
+
+	set_tick_and_enable_timer();
+	ENABLE_IRQ();
 }
 
 void sem_down(sem_s* sem) {
+	DISABLE_IRQ();
+
 	--(sem->counter);
 	// Add one process to the queue
+
+	set_tick_and_enable_timer();
+	ENABLE_IRQ();
 }
