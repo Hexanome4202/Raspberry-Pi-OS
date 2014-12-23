da6b5b3cd883eb7d3eeea003b62ca0a8f14bde8f
MartinRemi <mrmi11@yahoo.fr>
2014-12-23 14:58:29 +0100

    Updated semaphores

--- ospie-start/sem.c
+++ ospie-start/sem.c
@@ -2,6 +2,8 @@
 #include "sched.h"
 #include "hw.h"
 
+// TODO: change set_tick... to set normal waiting time or something smarter
+
 void sem_init(sem_s* sem, unsigned int val) {
 	sem->counter = val;
 }
@@ -19,9 +21,11 @@ void sem_up(sem_s* sem) {
 void sem_down(sem_s* sem) {
 	DISABLE_IRQ();
 
-	--(sem->counter);
-	// Add one process to the queue
-
+	if(sem->counter > 0) {
+		--(sem->counter);
+	} else {
+		// Add one process to the queue
+	}
 	set_tick_and_enable_timer();
 	ENABLE_IRQ();
 }
